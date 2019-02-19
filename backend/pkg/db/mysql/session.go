a05a328818db0f26975412b99937a78196b70d37
immabird <immapokeyew@gmail.com>
2019-02-19 18:46:53 -0500

    added get a session and get all sessions

--- backend/pkg/db/mysql/session.go
+++ backend/pkg/db/mysql/session.go
@@ -17,13 +17,67 @@ func NewSessionMySQL(db *sql.DB) SessionMySQL {
 }
 
 // ReadASession reads a session from the db given sessionID
-func (SessionMySQL) ReadASession(sessionID int) (db.Session, error) {
-	return db.Session{}, nil
+func (s SessionMySQL) ReadASession(sessionID int) (db.Session, error) {
+	if s.db == nil {
+		return db.Session{}, ErrDBNotSet
+	}
+
+	q := "SELECT * FROM session WHERE sessionID = " + string(sessionID) + ";"
+
+	rows, err := s.db.Query(q)
+	if err != nil {
+		return db.Session{}, err
+	}
+	defer rows.Close()
+
+	session := db.Session{}
+	hasNext := rows.Next()
+
+	if hasNext == true {
+		rows.Scan(&session.ID, &session.StartTime, &session.EndTime, &session.Title, &session.Speaker, &session.Room)
+	} else {
+		err = rows.Err()
+		if err != nil {
+			return session, err
+		}
+		return session, ErrNoRowsFound
+	}
+
+	hasNext = rows.Next()
+	if hasNext == true {
+		return session, ErrTooManyRows
+	}
+
+	return session, nil
 }
 
 // ReadAllSessions reads all sessions from the db
-func (SessionMySQL) ReadAllSessions() ([]db.Session, error) {
-	return []db.Session{}, nil
+func (s SessionMySQL) ReadAllSessions() ([]db.Session, error) {
+	if s.db == nil {
+		return nil, ErrDBNotSet
+	}
+
+	q := "SELECT * FROM session;"
+
+	rows, err := s.db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	sessions := []db.Session{}
+	for rows.Next() {
+		session := db.Session{}
+		rows.Scan(&session.ID, &session.StartTime, &session.EndTime, &session.Title, &session.Speaker, &session.Room)
+		sessions = append(sessions, session)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return sessions, nil
 }
 
 // WriteASession writes a session to the db
