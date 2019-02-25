77caf6f558568dd7c6ca49ffcd15a0123a2161ea
Bradfield <bradfielda@wit.edu>
2019-02-24 20:32:33 -0500

    made it a bit better

--- backend/pkg/db/mysql/room.go
+++ backend/pkg/db/mysql/room.go
@@ -18,7 +18,28 @@ func NewRoomMySQL(db *sql.DB) RoomMySQL {
 
 // ReadARoom reads a room from the db given roomName
 func (r RoomMySQL) ReadARoom(roomName string) (db.Room, error) {
-	return db.Room{}, nil
+	if r.db == nil {
+		return nil, ErrDBNotSet
+	}
+
+	q := "SELECT * FROM room WHERE roomName = ?;"
+	selectedRooms := db.Room{}
+
+	rows, err := r.db.Query(q, roomName)
+	if err != nil {
+		return selectedRooms, err
+	}
+
+	defer rows.Close()
+
+	for rows.next() {
+		selectedRooms = db.Room{
+			roomID: roomID;
+			roomName: roomName;
+			Capacity: Capacity;
+		}
+	}
+	return selectedRooms, nil
 }
 
 // ReadAllRooms reads all rooms from the db
@@ -46,15 +67,33 @@ func (r RoomMySQL) ReadAllRooms() ([]db.Room, error) {
 
 // WriteARoom writes a room to the db
 func (r RoomMySQL) WriteARoom(room db.Room) error {
+	if r.db == nil {
+		return nil, ErrDBNotSet
+	}
+
+	q := "" //I am not sure how to insert here based on what is being passed
+
 	return nil
 }
 
 // UpdateARoom updates a room in the db given a roomName and the updated room
 func (r RoomMySQL) UpdateARoom(roomName string, newRoom db.Room) error {
+	//Are we adding more parameters here as well?
 	return nil
 }
 
 // DeleteARoom deletes a room given a roomName
 func (r RoomMySQL) DeleteARoom(roomName int) error {
+	if r.db == nil {
+		return nil, ErrDBNotSet
+	}
+
+	q := "DELETE FROM room WHERE roomName = ?"
+	rows, err := r.db.Query(q, roomName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
 	return nil
 }
