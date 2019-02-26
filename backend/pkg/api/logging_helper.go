e709c5306fcf459125b1beed1c14babcd1f8a8ef
immabird <immapokeyew@gmail.com>
2019-02-26 17:07:32 -0500

    return json error message and accept int http status

--- backend/pkg/api/logging_helper.go
+++ backend/pkg/api/logging_helper.go
@@ -1,13 +1,15 @@
 package api
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 // ReportError logs the error and respond with message
-func ReportError(err error, msg string, w http.ResponseWriter) {
-	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(msg))
+func ReportError(err error, msg string, httpStatusCode int, w http.ResponseWriter) {
+	w.WriteHeader(httpStatusCode)
+	jsonMsg, _ := json.Marshal(msg)
+	w.Write([]byte(jsonMsg))
 	log.Printf("%s, %v\n", msg, err)
 }
