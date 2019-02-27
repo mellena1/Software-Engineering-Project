ad34f99e18cfacbb164c6e4f02b0ad2ea22fae97
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-02-27 10:36:02 -0500

    more sessions

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent }   from './pages/dashboard/dashboard.component';
+import { SessionsComponent }   from './pages/sessions/sessions.component';
 import { RoomsComponent } from './pages/rooms/rooms.component';
 import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { TimeslotsComponent } from './pages/timeslots/timeslots.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/sessions', pathMatch: 'full' },
+  { path: 'sessions', component: SessionsComponent },
   { path: 'rooms', component: RoomsComponent},
   { path: 'speakers', component: SpeakersComponent},
   { path: 'timeslots', component: TimeslotsComponent}
--- frontend/src/app/pages/sessions/sessions.component.ts
+++ frontend/src/app/pages/sessions/sessions.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  selector: 'app-sessions',
+  templateUrl: './sessions.component.html',
+  styleUrls: [ './sessions.component.css' ]
 })
-export class DashboardComponent implements OnInit {
+export class SessionsComponent implements OnInit {
 
   ngOnInit() {
 
