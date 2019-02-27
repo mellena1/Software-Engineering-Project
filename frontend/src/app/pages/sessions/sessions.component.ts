ce7259bce9daee93779a6b73de94b5b4bdda927b
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-02-27 16:51:03 -0500

    component
    started to fill in components

--- frontend/src/app/pages/sessions/sessions.component.ts
+++ frontend/src/app/pages/sessions/sessions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../services/session.service'
+import { Session} from '../../data_models/session'
 
 @Component({
   selector: 'app-sessions',
@@ -6,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './sessions.component.css' ]
 })
 export class SessionsComponent implements OnInit {
+  sessions: Session[];
+  selectedSession: Session;
+  error: any;
+  constructor(private sessionService: SessionService ) { }
+
 
   ngOnInit() {
-
   }
+
+  getAllSessions(): void {
+    this.sessionService
+      .getAllSessions()
+      .subscribe(
+        sessions => (this.sessions = sessions),
+        error => (this.error = error)
+      )
+  }
+
+  onSelect(session: Session): void {
+    this.selectedSession = session;
+  }
+
 }
