d0154bb4fccd8aa7e235a5420f640fa512f4dc05
immabird <immapokeyew@gmail.com>
2019-02-26 19:50:56 -0500

    added test stubs

---
+++ backend/pkg/db/mysql/session_test.go
@@ -0,0 +1,15 @@
+package mysql
+
+import "testing"
+
+func TestReadAllSessionsValid(t *testing.T) {
+}
+
+func TestReadAllSessionsInvalid(t *testing.T) {
+}
+
+func TestReadASessionValid(t *testing.T) {
+}
+
+func TestReadASessionInvalid(t *testing.T) {
+}
