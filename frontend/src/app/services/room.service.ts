a53ec6bee6c2eba911acf79136c90692bc618925
robertmdilorenzo <robertmdilorenzo@gmail.com>
2019-02-27 20:24:54 -0500

    First try for room service
    Working on Getting a room by ID and sending the id info with httpclient

--- frontend/src/app/services/room.service.ts
+++ frontend/src/app/services/room.service.ts
@@ -1,31 +1,48 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable, throwError as observableThrowError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators'
 
 import { Room } from '../data_models/room'
+import { stringify } from '@angular/core/src/render3/util';
+import { headersToString } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
   private apiUrl = environment.apiUrl;
+  
+  
 
   constructor(private http: HttpClient) { }
+  headers = new HttpHeaders();
+
+  
+  
+
 
   getAllRooms() {
     return this.http
-      .get<Room[]>(this.apiUrl + '/room')
+      .get<Room[]>(this.apiUrl + '/rooms')
       .pipe(map(data => data), catchError(this.handleError));
   }
 
-  getARoom(id: number) {
+  getARoom(id: number) :Observable<Room>{
+    const data = new FormData();
+    this.headers.append('id', 'number');
+    this.headers.set('id', stringify(id));
+    data.append('id', stringify(id));
+
+    return this.http
+    .get<Room>(this.apiUrl + '/room', data)
+    .pipe(map(data=>data), catchError(this.handleError));
     
   }
 
-  writeRoom() {
-
+  writeRoom(room: Room) {
+    return this.http.get(this.apiUrl + '/room')
   }
 
   updateRoom() {
