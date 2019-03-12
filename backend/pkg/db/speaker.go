16a425d47ada384d358a859073197726c2d57c77
Andrew Mellen <andrew_mellen@icloud.com>
2019-03-12 16:05:31 -0400

    Add ID tag for speaker

--- backend/pkg/db/speaker.go
+++ backend/pkg/db/speaker.go
@@ -2,7 +2,7 @@ package db
 
 // Speaker holds all data about a speaker
 type Speaker struct {
-	ID        int64
+	ID        int64   `json:"id" example:"1"`
 	Email     *string `json:"email" example:"firstname.lastname@gmail.com"`
 	FirstName *string `json:"firstName" example:"Bob"`
 	LastName  *string `json:"lastName" example:"Smith"`
