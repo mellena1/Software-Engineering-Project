c91c2c012760f8d9b9cbe118e8a43fab2ba75408
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-03-14 14:49:43 -0400

    Session Page (Create, Read, Delete) (#77)
    * sessions component updated
    
    * updated look
    
    updated with table look, trying to get write functionality
    
    * working on displaying sessions + populating dropdowns
    
    * implemented timeslot
    
    implemented timeslot and started working on edit
    
    * onSubmit + dropdown stuff
    
    wrote up an onSubmit for Sessions and made some progress with dropdowns, just need to figure out how to get our info into them instead of junk
    
    * display working
    
    displays details properly, does not display dropdowns properly
    
    * display complete
    
    everything displays properly now, just working on add and edit and it should be complete (delete works fine seemingly)
    
    * ngSubmit call
    
    * add button works now
    
    add button working, need to fix error but it is calling onSubmit now
    
    * POST error fix + cleanup
    
    fixed a bug in the service that was causing a POST error and fixed some form syntax in the html to improve status of Add functionality
    
    * Inline edit partially working
    
    Working on implementing inline edit
    
    * Add other service stuff
    
    * edit functionality started
    
    implementing what the edit team has started on for session, going to try and fix adding session name
    
    * Fix obj to send for session
    
    * Update sessions.component.html
    
    * fixed update function
    
    * Everything except update is working
    
    I got everything to work except when the "save" button is clicked, it wipes out all the data in the selected timeslot and doesn't update.
    
    * fixed write and update services afaik, working on update functionality
    
    * prettier

--- frontend/src/app/pages/sessions/sessions.component.ts
+++ frontend/src/app/pages/sessions/sessions.component.ts
@@ -1,46 +1,57 @@
 import { Component, OnInit, NgModule } from "@angular/core";
+
 import { SessionService } from "../../services/session.service";
+import { RoomService } from "../../services/room.service";
+import { SpeakerService } from "../../services/speaker.service";
+import { TimeslotService } from "../../services/timeslot.service";
+
 import { Session } from "../../data_models/session";
-import { Room } from "src/app/data_models/room";
-import { Speaker } from "src/app/data_models/speaker";
-import { Timeslot } from "src/app/data_models/timeslot";
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  ReactiveFormsModule
-} from "@angular/forms";
-import { FormsModule } from "@angular/forms";
+import { Room } from "../../data_models/room";
+import { Speaker } from "../../data_models/speaker";
+import { Timeslot } from "../../data_models/timeslot";
+
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-sessions",
   templateUrl: "./sessions.component.html",
   styleUrls: ["./sessions.component.css"]
 })
-@NgModule({
-  imports: [FormsModule, ReactiveFormsModule],
-  declarations: [SessionsComponent]
-})
 export class SessionsComponent implements OnInit {
-  constructor(private sessionService: SessionService) {}
+  constructor(
+    private sessionService: SessionService,
+    private roomService: RoomService,
+    private speakerService: SpeakerService,
+    private timeslotService: TimeslotService
+  ) {}
   sessions: Session[];
-  newSession: Session;
+  rooms: Room[];
+  speakers: Speaker[];
+  timeslots: Timeslot[];
+  session = new Session(
+    "",
+    new Room("", 0),
+    new Speaker("", "", ""),
+    new Timeslot("", "")
+  );
   selectedSession: Session;
   error: any;
-  sessionForm: FormGroup;
+  isEditable: boolean;
+  isCurrent: boolean;
+  sessionForm = new FormGroup({
+    sessionName: new FormControl(""),
+    sessionRoom: new FormControl(""),
+    sessionSpeaker: new FormControl(""),
+    sessionTimeslot: new FormControl("")
+  });
 
   ngOnInit() {
     this.getAllSessions();
-    this.sessionForm = new FormGroup({
-      sessionName: new FormControl(this.newSession.name, [Validators.required]),
-      sessionRoom: new FormControl(this.newSession.room, [Validators.required]),
-      sessionSpeaker: new FormControl(this.newSession.speaker, [
-        Validators.required
-      ]),
-      sessionTime: new FormControl(this.newSession.timeslot, [
-        Validators.required
-      ])
-    });
+    this.getAllRooms();
+    this.getAllSpeakers();
+    this.getAllTimeslots();
+    this.isEditable = false;
+    this.isCurrent = true;
   }
 
   getAllSessions(): void {
@@ -52,7 +63,91 @@ export class SessionsComponent implements OnInit {
       );
   }
 
+  getAllRooms(): void {
+    this.roomService
+      .getAllRooms()
+      .subscribe(rooms => (this.rooms = rooms), error => (this.error = error));
+  }
+
+  getAllSpeakers(): void {
+    this.speakerService
+      .getAllSpeakers()
+      .subscribe(
+        speakers => (this.speakers = speakers),
+        error => (this.error = error)
+      );
+  }
+
+  getAllTimeslots(): void {
+    this.timeslotService
+      .getAllTimeslots()
+      .subscribe(
+        timeslots => (this.timeslots = timeslots),
+        error => (this.error = error)
+      );
+  }
+
+  getSession(id: number) {
+    this.sessionService.getSession(id);
+  }
+
+  updateSession(updatedSession: Session): void {
+    console.log("Yo");
+    if (confirm("Are you sure you want to update?")) {
+      this.sessionService
+        .updateSession(updatedSession)
+        .subscribe(error => (this.error = error));
+      console.log("The following Session Udpated :", this.sessionForm.value);
+      this.sessions = this.sessions.filter(item => item !== updatedSession);
+      this.sessionForm.reset();
+      this.sessions.push(updatedSession);
+    }
+  }
+
+  deleteSession(id: number) {
+    if (confirm("Are you sure you want to remove it?")) {
+      this.sessionService
+        .deleteSession(id)
+        .subscribe(error => (this.error = error));
+      console.log("The following Session Deleted :", this.sessionForm.value);
+      this.sessions = this.sessions.filter(item => item.id !== id);
+    }
+  }
+
   onSelect(session: Session): void {
     this.selectedSession = session;
+  }
+
+  onSubmit(): void {
+    var newSession = new Session(
+      this.session.name,
+      this.session.room,
+      this.session.speaker,
+      this.session.timeslot
+    );
+    this.sessionService
+      .writeSession(
+        this.session.name,
+        this.session.room.id,
+        this.session.speaker.id,
+        this.session.timeslot.id
+      )
+      .subscribe(
+        response => (newSession.id = response.id),
+        error => (this.error = error)
+      );
+    console.log("Session Submitted!", this.sessionForm.value);
+    this.sessionForm.reset();
+    this.sessions.push(newSession);
+  }
+
+  showEdit(session: Session): void {
+    console.log("Showing Edit");
+    session.isEditable = true;
+  }
+
+  cancel(session: Session): void {
+    console.log("Showing Cancel");
+    session.isEditable = false;
   }
 }
