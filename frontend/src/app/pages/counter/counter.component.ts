1f655c4908d18fb28a4d3ab681cbbcdbcfdf7755
adam7397 <adamsbradfield@gmail.com>
2019-04-02 09:15:43 -0400

    fixed syntax in counter component

--- frontend/src/app/pages/counter/counter.component.ts
+++ frontend/src/app/pages/counter/counter.component.ts
@@ -4,65 +4,56 @@ import { Count } from "src/app/data_models/count";
 import { CountService } from "src/app/services/count.service";
 
 @Component({
-	selector: "",
-	templateUrl: "./counter.component.html",
-	styleUrls: ["./counter.component.css"]
+  selector: "",
+  templateUrl: "./counter.component.html",
+  styleUrls: ["./counter.component.css"]
 })
-
 export class CounterComponent implements OnInit {
-	constructor(private countService: CountService) {}
-	username: //need to set username here from login
-	error: any;
+  constructor(private countService: CountService) {}
+  username: document.cookie; //need to set username here from login
+  error: any;
 
-	countForm = new FormGroup({
-		userName: //passed in
-		count: new FormControl(""),
-		time: new //going to be button, not form
-		sessionID: new //this will be passed as well
-	});
+  countForm = new FormGroup({
+    userName: document.cookie, //passed in
+    count: new FormControl(""),
+    time: new FormControl(""), //going to be button, not form
+    sessionID: new formControl("") //this will be passed as well
+  });
 
-	getACount(sessionID): void {
-	  this.CountService()
-	  .getACount(sessionID)
-	  .subscribe(
-	  	count => (this.count = count)
-	  	error => (this.error = error)
-	  	);
-	}
+  getACount(sessionID): void {
+    this.CountService()
+      .getACount(sessionID)
+      .subscribe(count => (this.count = count), error => (this.error = error));
+  }
 
-	writeACount(): void {
-	  var newCount = new Count(
-	  	this.count.count,
-	  	this.count.username,
-	  	this.count.sessionID,
-	  	this.count.time
-	  );
-	  this.countService
-	  	.writeACount(
-	  	  this.count.count,
-	  	  this.count.username,
-	  	  this.count.sessionID,
-	  	  this.count.time
-	  	)
-	  	.subscribe(
-	  	  response => {
-	  	  	this.countForm.reset();
-	  	  	console.log("Count Submitted");
-	  	  },
-	  	  error => {
-	  	  	this.error = error;
-	  	  	console.log(this.error);
-	  	  }
-	  	);
-	}
+  writeACount(): void {
+    var newCount = new Count(
+      this.count.count,
+      this.count.username,
+      this.count.sessionID,
+      this.count.time
+    );
+    this.countService
+      .writeACount(
+        this.count.count,
+        this.count.username,
+        this.count.sessionID,
+        this.count.time
+      )
+      .subscribe(
+        response => {
+          this.countForm.reset();
+          console.log("Count Submitted");
+        },
+        error => {
+          this.error = error;
+          console.log(this.error);
+        }
+      );
+  }
 
-	updateCount(): void { //not sure where to take this
-		var index = this.count.findIndex(
-			item => item.id
-		);
-
-
-	}
-
-
+  updateCount(): void {
+    //not sure where to take this
+    var index = this.count.findIndex(item => item.id);
+  }
 }
