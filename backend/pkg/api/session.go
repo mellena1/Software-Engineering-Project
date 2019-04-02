76418e4862471eb58959d35f0b21506e0733f25c
Kenny Robinson <robinsonk2@wit.edu>
2019-04-01 21:22:50 -0400

    Fix tiny things
    -fixes formatting issues that arose during merge conflict resolution
    
    -fixes swagger doc warning
    
    -updates swagger docs to be more specific regarding the sorting api endpoints

--- backend/pkg/api/session.go
+++ backend/pkg/api/session.go
@@ -34,7 +34,6 @@ func CreateSessionRoutes(sessionDBFacade db.SessionReaderWriterUpdaterDeleter) [
 		NewRoute("/api/v1/session", sessAPI.deleteASession, "DELETE"),
 		NewRoute("/api/v1/sessionsBySpeaker", sessAPI.getAllSessionsBySpeaker, "GET"),
 		NewRoute("/api/v1/sessionsByTimeslot", sessAPI.getAllSessionsByTimeslot, "GET"),
-
 	}
 
 	return routes
@@ -108,7 +107,6 @@ func (r WriteASessionRequest) Validate() error {
 	return nil
 }
 
-
 // @Summary Get all sessions, sorted by speaker
 // @Description Return a list of all sessions sorted by speaker
 // @Produce json
@@ -117,7 +115,7 @@ func (r WriteASessionRequest) Validate() error {
 // @Failure 503 {} _ "failed to access the db"
 // @Router /api/v1/sessionsBySpeaker [get]
 func (s sessionAPI) getAllSessionsBySpeaker(w http.ResponseWriter, r *http.Request) {
-  sessions, err := s.sessionReader.ReadAllSessions()
+	sessions, err := s.sessionReader.ReadAllSessions()
 	if err != nil {
 		ReportError(err, "Failed to get all session", http.StatusBadRequest, w)
 		return
@@ -129,19 +127,18 @@ func (s sessionAPI) getAllSessionsBySpeaker(w http.ResponseWriter, r *http.Reque
 	}
 
 	jjson, err := json.Marshal(sessionsBySpeaker)
-  if err != nil {
+	if err != nil {
 		ReportError(err, "Failed to marshal data", http.StatusBadRequest, w)
 		return
 	}
-  w.Write(jjson)
+	w.Write(jjson)
 }
 
-
-// getAllSessions Gets all sessions from the db
-// @Summary Get all sessions
-// @Description Return a list of all sessions
+// getAllSessions Gets all sessions from the db, sorted by timeslot
+// @Summary Get all sessions, sorted by timeslot
+// @Description Return a list of all sessions, sorted by timeslot
 // @Produce json
-// @Success 200 {array} map[string][]*string
+// @Success 200 {array} string "An array of timeslotString: Sessions map entries"
 // @Failure 400 {} _ "the request was bad"
 // @Failure 503 {} _ "failed to access the db"
 // @Router /api/v1/sessionsByTimeslot [get]
@@ -164,7 +161,7 @@ func (s sessionAPI) getAllSessionsByTimeslot(w http.ResponseWriter, r *http.Requ
 		ReportError(err, "Failed to marshal data", http.StatusBadRequest, w)
 		return
 	}
-  w.Write(j)
+	w.Write(j)
 }
 
 // writeASession Add a session to the db
