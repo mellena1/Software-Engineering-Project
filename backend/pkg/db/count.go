633a47f6aed5a5e4824923d991b36604b64d1e25
Kenny Robinson <39377887+robinsonk2@users.noreply.github.com>
2019-04-09 15:33:22 -0400

    Check out this PHASE 3 ENPOINT bruh (#102)
    * Check out this PHASE 3 ENPOINT bruh
    
    -im awesome
    -didn't think i'd figure it out but eyyy
    -lol jk theres still a little bug, ill keep tryna figure out
    
    -so, basically, this returns a json map of <speaker, map of <sessioName, Count array>>
    
    -the bug is that I'm creating a new <sessionName, Count array> map each iteration of looping through the data, and similar values are able to be appended to the speaker key, so it doesn't break anything as is. just gotta figure it out
    
    -figured i'd push this so its there for people to see/view/approve/merge/pull/workwith
    
    -k thnx
    
    * Fixed the JSON bug WOO
    
    Alrighty - this is ready to commit and use
    
    -its very pretty
    
    -have fun
    
    * Small formatting Fixes
    
    -Resolves formatting issues mentioned in comments and some that arose after resolving merge conflicts

--- backend/pkg/db/count.go
+++ backend/pkg/db/count.go
@@ -18,6 +18,30 @@ func NewCount() Count {
 	}
 }
 
+// CountBySpeakerResponse response struct for phase 3
+type CountBySpeakerResponse struct {
+	SessionName      *string `json:"time" example:"Microservices"`
+	SpeakerFirstName *string `json:"speakerFirstName" example:"Kenny"`
+	SpeakerLastName  *string `json:"speakerLastName" example:"Robinson"`
+	Time             *string `json:"countTime" example:"beginning/middle/end"`
+	SessionID        *int64  `json:"SessionID" example:"1"`
+	UserName         *string `json:"userName" example:"Kenny Robinson"`
+	Count            *int64  `json:"count" example:"30"`
+}
+
+// NewCountBySpeaker makes a new Count with default values
+func NewCountBySpeaker() CountBySpeakerResponse {
+	return CountBySpeakerResponse{
+		SpeakerFirstName: StringPtr(""),
+		SpeakerLastName:  StringPtr(""),
+		SessionName:      StringPtr(""),
+		Time:             StringPtr(""),
+		SessionID:        Int64Ptr(0),
+		UserName:         StringPtr(""),
+		Count:            Int64Ptr(0),
+	}
+}
+
 // CountReaderWriterUpdaterDeleter implements everything that a facade for a Count would need
 type CountReaderWriterUpdaterDeleter interface {
 	CountReader
@@ -29,6 +53,7 @@ type CountReaderWriterUpdaterDeleter interface {
 // CountReader implements all read related methods
 type CountReader interface {
 	ReadCountsOfSession(sessionID int64) ([]Count, error)
+	ReadAllCountsBySpeaker() ([]CountBySpeakerResponse, error)
 	ReadAllCounts() ([]Count, error)
 }
 
