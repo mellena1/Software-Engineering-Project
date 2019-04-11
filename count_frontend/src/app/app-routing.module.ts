8510b83741d68badd4b592700cc91b218d11297b
Charles Arnaudo <charlesarnaudo@gmail.com>
2019-04-11 13:57:40 -0400

    fix login routing (#111)

--- count_frontend/src/app/app-routing.module.ts
+++ count_frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { CountComponent } from "./pages/count/count.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/count", pathMatch: "full" },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
   { path: "count", component: CountComponent }
 ];
 
